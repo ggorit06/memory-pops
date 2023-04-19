212adcedafe3aec58afbb5b475b2037a5a5013c8
Genevieve Gorit <99786874+ggorit06@users.noreply.github.com>
2023-04-19 13:21:26 -0500

    Initial commit

---
+++ src/Note.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Note = (props) => {
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+
+  const clickDelete = () => props.removeNote(props.note.id);
+
+  const addDateTime = () => {
+    const now = new Date();
+    const dateTime = now.toLocaleString();
+    const textArea = document.querySelector("#sticky-note textarea");
+    const currentText = textArea.value;
+    const newText = currentText + "\n" + dateTime;
+    textArea.value = newText;
+  };
+
+  return (
+    <li className="note">
+      <input
+        type="text"
+        value={props.note.title}
+        onChange={updateTitle}
+        placeholder="Title"
+        className="note__title"
+      />
+      <textarea
+        value={props.note.description}
+        onChange={updateDescription}
+        placeholder="Description..."
+        className="note__description"
+      />
+      <span onClick={clickDelete} className="note__delete">
+        X
+      </span>
+      <div id="sticky-note">
+        <textarea></textarea>
+        <button onClick={addDateTime}>Add Date and Time</button>
+      </div>
+    </li>
+  );
+};
+
+export default Note;
